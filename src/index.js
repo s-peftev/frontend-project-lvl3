c627da97de6f4f26bdc33b0f3d82971a6abc0ab5
s-peftev <s.peftev@gmail.com>
2022-04-13 11:12:19 +0300

    setup and html base

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+import 'bootstrap';
+import _ from 'lodash';
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+import path, { dirname } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    // new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+};
